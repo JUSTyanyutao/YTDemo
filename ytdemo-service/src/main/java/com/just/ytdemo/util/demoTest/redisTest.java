03d2e6010268041254deb5495f7e7a8ede56a207
yutao.yan <yutao.yan@yintec.cn>
2021-01-23 19:47:02 +0800

    fix bug

---
+++ ytdemo-service/src/main/java/com/just/ytdemo/util/demoTest/redisTest.java
@@ -0,0 +1,40 @@
+package com.just.ytdemo.util.demoTest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class redisTest {
+
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+
+    public void listTest(){
+
+//        redisTemplate.opsForList().
+        redisTemplate.getConnectionFactory().getConnection().publish()
+        redisTemplate.boundListOps("yt:test:list").leftPush();
+
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+
+
+
+
+
+}
