9d818f5c717a4f4e3466ddd5845689fef7dcdc53
yutao.yan <yutao.yan@yintec.cn>
2021-04-26 19:43:45 +0800

    aop  日志

--- ytdemo-service-common/src/main/java/com/just/ytdemo/aspect/LogAspect.java
+++ ytdemo-service-common/src/main/java/com/just/ytdemo/aspect/LogAspect.java
@@ -1,8 +1,14 @@
 package com.just.ytdemo.aspect;
 
+import com.alibaba.fastjson.JSON;
+import javafx.scene.paint.Stop;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 
 
 /**
@@ -15,6 +21,7 @@ import org.springframework.stereotype.Component;
  * 环绕通知 @After
  *
  */
+@Slf4j
 @Aspect
 @Component
 public class LogAspect {
@@ -28,42 +35,93 @@ public class LogAspect {
     @Pointcut("execution(* com.just.ytdemo.export..*.*(..))")
     public void pointCut(){}
 
-    /**
-     * 前置通知
-     */
-    @Before("pointCut()")
-    public void beforeLog(){
-        System.out.println("before log");
-    }
-
-    /**
-     * 后置通知
-     */
-    @After("pointCut()")
-    public void afterLog(){
-        System.out.println("after log");
-    }
-
-
-    /**
-     * 返回通知
-     */
-    @AfterReturning("pointCut()")
-    public void afterReturnLog(){
-        System.out.println("afterReturn log");
-    }
-
-
 //    /**
-//     * 环绕通知
+//     * 前置通知
 //     */
-//    @Around("pointCut()")
-//    public void aroundLog(JoinPoint joinPoint){
+//    @Before("pointCut()")
+//    public void beforeLog(){
+//        System.out.println("before log");
+//    }
+//
+//    /**
+//     * 后置通知
+//     */
+//    @After("pointCut()")
+//    public void afterLog(){
+//        System.out.println("after log");
+//    }
 //
 //
-//
-//        System.out.println("around log");
+//    /**
+//     * 返回通知
+//     */
+//    @AfterReturning("pointCut()")
+//    public void afterReturnLog(){
+//        System.out.println("afterReturn log");
 //    }
 
+
+    /**
+     * 环绕通知
+     */
+    @Around("pointCut()")
+    public Object aroundLog(ProceedingJoinPoint joinPoint){
+
+        Object[] args = joinPoint.getArgs();
+        String argsBuilder = getString(args);
+
+        // 自定义注解
+        if (joinPoint.getSignature() instanceof MethodSignature) {
+        }
+
+        log.info("{\"class\":"  + joinPoint.getTarget().getClass().getSimpleName()  +
+                "\"method\":"  + joinPoint.getSignature().getName()  +
+                "\"params\":}" +  argsBuilder  +
+                "}");
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        Object obj;
+        try {
+            obj = joinPoint.proceed();
+            String response = null ;
+            if (obj != null) {
+                response = getString(obj);
+            }
+
+            stopWatch.stop();
+            log.info(
+                    "{\"class\":" + joinPoint.getTarget().getClass().getSimpleName() +
+                    "\"method\":" + joinPoint.getSignature().getName() +
+                    ",\"input\":" + argsBuilder +
+                    ",\"output\":" + response +
+                    ",\"cost\":" + (stopWatch.getTotalTimeMillis()) +
+                    "}");
+            return obj;
+        } catch (Throwable throwable) {
+            log.error(
+                    "{\"class\":" + joinPoint.getTarget().getClass().getSimpleName() +
+                            "\"method\":" + joinPoint.getSignature().getName() +
+                            ",\"input\":" + argsBuilder +
+                            ",\"cost\":" + (stopWatch.getTotalTimeMillis()) +
+                            "}");
+            return "系统异常";
+        }
+
+    }
+
+
+    private String getString(Object arg) {
+        String s;
+        try {
+            s = JSON.toJSONString(arg);
+        } catch (Exception e) {
+            s = "can't parse to JSON, " + e.getMessage();
+        }
+        if (s.length() > 550) {
+            s = String.format("%s....{%s}....%s", s.substring(0, 250), s.length(), s.substring(s.length() - 250, s.length()));
+        }
+        return s;
+    }
 
 }
